d1167c40adf5b22fe7410033aeeb2ed9cd3ad965
Alexey Osipenko <alexey@osipenko.in.ua>
2014-06-08 16:54:40 +0300

    remove unnec. code

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@ company = Company.create name: 'Ghost'
 
 User.create email: 'example@example.com', password: 'epyfnm', password_confirmation: 'epyfnm', company: company
 
-project = Project.create name: 'sample ghost blog', company: company
+Project.create name: 'sample ghost blog', company: company
 Project.create name: 'another project', company: company
 Project.create name: 'yet another project', company: company
