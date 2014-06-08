d032020e9440bdd788d95b47ca75881f60bbfaee
Alexey Osipenko <alexey@osipenko.in.ua>
2014-06-08 12:51:08 +0300

    fix seed according to password confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 company = Company.create name: 'Ghost'
 
-User.create email: 'example@example.com', password: 'epyfnm', company: company
+User.create email: 'example@example.com', password: 'epyfnm', password_confirmation: 'epyfnm', company: company
 
 project = Project.create name: 'sample ghost blog', company: company
 Project.create name: 'another project', company: company
