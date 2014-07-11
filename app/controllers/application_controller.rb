5e93050e1910cf08a952953939fdf297147a070c
Alexey Osipenko <alexey@osipenko.in.ua>
2014-07-11 11:46:07 +0300

    make resoure_url_helper more configurable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include ::Clearance::Controller
   include ::ResourceUrlHelper
+
+  # polymorphic_url_prefix do
+  #   {
+  #     User => ->(resource) { [resource.company, resource] }
+  #   }
+  # end
+
 end
--- app/controllers/concerns/resource_url_helper.rb
+++ app/controllers/concerns/resource_url_helper.rb
@@ -6,16 +6,39 @@ module ResourceUrlHelper
     helper_method :polymorphic_url
   end
 
-  RESOURCES = {
-    User => ->(resource) { [resource.company, resource] },
-    # SubAccount => ->(resource) { [resource.account, resource] },
-    # Backlink => ->(resource) { [resource.article.network, resource.article, resource] },
-    # Page => ->(resource) { [resource.campaign, resource] },
-  }
-  RESOURCES.default = ->(resource) { resource }
+  class Resource
+
+    def initialize(values={}, default = ->(resource) { resource })
+      @resources = values.dup
+      @resources.default = default
+    end
+
+    def get(record)
+      @resources[record.class][record]
+    end
+
+  end
+
+  module ClassMethods
+    def polymorphic_url_prefix(content = nil)
+      content_proc = if block_given?
+                       Proc.new
+                     else
+                       Proc.new { content }
+                     end
+      define_method :polymorphic_url_prefix do
+        @polymorphic_url_prefix ||= Resource.new(*Array.wrap(instance_exec(&content_proc)))
+      end
+    end
+  end
 
   def polymorphic_url(record_or_hash_or_array, options={})
-    super(RESOURCES[record_or_hash_or_array.class][record_or_hash_or_array], options)
+    parameters = Array.wrap(record_or_hash_or_array).map {|i| polymorphic_url_prefix.get(i) }.flatten
+    super(parameters, options)
+  end
+
+  def polymorphic_url_prefix
+    @polymorphic_url_prefix ||= Resource.new()
   end
 
 end
