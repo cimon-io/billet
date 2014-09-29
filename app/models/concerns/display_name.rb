1b2011bc645f9931af8a4e0c8a2c854c36ed8369
Alexey Osipenko <alexey@osipenko.in.ua>
2014-09-29 16:49:21 +0300

    display_name in process

--- app/models/concerns/display_name.rb
+++ app/models/concerns/display_name.rb
@@ -2,16 +2,28 @@ module DisplayName
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def display_name(alias_method)
+    def display_name(alias_method=nil)
+      content_proc = block_given? ? Proc.new : Proc.new { self.public_send(alias_method) }
       define_method(:display_name) do
-        self.public_send(alias_method).presence || self.default_display_name
+        instance_exec(&content_proc).presence || self.display_name_backward
       end
     end
   end
 
-  def default_display_name
-    return "#{self.class.model_name.human} #{self.id}" if self.id
-    return "New #{self.class.model_name.human}"
+  def display_name_backward
+    self.public_send(self.display_name_assumption)
+  end
+
+  def display_name_assumptions
+    %w{full_name name title subject default_display_name}
+  end
+
+  def display_name_assumption
+    Array.wrap(self.display_name_assumptions).find { |method_name| self.respond_to?(method_name) }
+  end
+
+  def display_name_default
+    self.id ? "#{self.class.model_name.human} #{self.id}" : "New #{self.class.model_name.human}"
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,9 @@ class User < ActiveRecord::Base
   include Clearance::User
   include ::PasswordConfirmation
   include ::EmailConfirmationGuard::User
-  display_name :email_name
+  display_name { self.email.to_s.split('@').first }
 
   belongs_to :company
-
-  def email_name
-    self.email.to_s.split('@').first
-  end
 
   def config
     Settings.default_user_config
