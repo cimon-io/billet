690c3509e1279cfd2f4e86c4a82b869117d5afbc
Alexey Osipenko <alexey@osipenko.in.ua>
2014-10-11 09:29:13 +0300

    display_name is able to use parent class

--- app/helpers/display_name_helper.rb
+++ app/helpers/display_name_helper.rb
@@ -5,7 +5,12 @@ module DisplayNameHelper
   RESOURCES.default = ->(resource) { resource.display_name }
 
   def display_name(resource)
-    self.instance_exec resource, &RESOURCES[resource.class]
+    display_name_keys = ->(klass) {
+      return [] if klass == ActiveRecord::Base
+      display_name_keys[klass.superclass] + Array.wrap(klass)
+    }
+    key = display_name_keys[resource.class].find { |klass| RESOURCES.key?(klass) }
+    self.instance_exec resource, &RESOURCES[key]
   end
 
 end
