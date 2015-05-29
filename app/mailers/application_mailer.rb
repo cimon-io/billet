a6982bc13e0d810175a31f168f161e99122206d4
Alexey Osipenko <alexey@osipenko.in.ua>
2015-05-29 12:14:21 +0300

    application_mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,14 @@ class ApplicationMailer < ActionMailer::Base
   default :template_path => Proc.new { self.class.name.gsub(/Mailer$/, "").underscore }
   prepend_view_path ['app/views/mailers']
 
+  # TODO: I belive that helpers should work from the box
+  helper :application, :display_name
+
+  protected
+
+  def echo(resource)
+    ApplicationDrapper.decorate(resource).blank_display_name
+  end
+
+
 end
