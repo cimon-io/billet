0f97e58cc8308bb11d0ebe5daea4701dbffb06a5
Alexey Osipenko <alexey@osipenko.in.ua>
2015-06-03 13:57:18 +0300

    has_peper_trail

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  has_paper_trail
   belongs_to :company
 
   validates :name, presence: true, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_paper_trail
   include Clearance::User
   include ::PasswordConfirmation
   include ::EmailConfirmationGuard::User
