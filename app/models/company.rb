5e7a77b3c44a779f5b1d6ca1ed0cbfe564c192c9
Alexey Osipenko <alexey@osipenko.in.ua>
2015-06-05 23:13:04 +0300

    inverse of

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,8 @@
 class Company < ActiveRecord::Base
   has_paper_trail
-  has_many :company_users, -> { with_default_order }
-  has_many :projects, -> { with_default_order }
+
+  has_many :company_users, -> { with_default_order }, inverse_of: :company
+  has_many :projects, -> { with_default_order }, inverse_of: :company
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -1,8 +1,8 @@
 class CompanyUser < ActiveRecord::Base
   acts_as_paranoid
 
-  belongs_to :user
-  belongs_to :company
+  belongs_to :user, inverse_of: :company_users
+  belongs_to :company, inverse_of: :company_users
 
   scope :with_default_order, -> { order(created_at: :asc) }
 
