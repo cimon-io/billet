44c9650980ff264050feda6fc5e843358de35e51
Sergey Konev <sergey@cimon.io>
2018-04-15 19:04:24 +0300

    FactoryBot updates

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,7 +2,7 @@ Rails.application.config.generators do |g|
   g.template_engine :haml
   g.helper false
   g.assets false
-  g.factory_girl false
+  g.factory_bot false
   g.test_framework :rspec, fixture: false,
     view_specs: false,
     controller_specs: false
