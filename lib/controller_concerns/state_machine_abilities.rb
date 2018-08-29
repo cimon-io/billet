cb922e096101c557a448ac02327fcdfb00299ccc
Alexey Osipenko <alexey@osipenko.in.ua>
2018-08-29 19:48:21 +0300

    Style/RedundantParentheses

--- lib/controller_concerns/state_machine_abilities.rb
+++ lib/controller_concerns/state_machine_abilities.rb
@@ -4,7 +4,7 @@ module StateMachineAbilities
   module ClassMethods
     def authorize_resource_state_events(opts = {})
       key = opts.delete(:key) { :state_event }
-      filter_params = ({ only: [:update] }).merge(opts)
+      filter_params = { only: [:update] }.merge(opts)
 
       before_filter(filter_params) do
         unless params[permitted_params_key][key].nil?
