8ab13e073c315e0197141ed32c05f6af497c9cd0
Alexey Osipenko <alexey@osipenko.in.ua>
2018-08-29 19:49:05 +0300

    Style/PercentLiteralDelimiters

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Billet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/lib/controller_concerns)
+    config.autoload_paths += %W[#{config.root}/lib #{config.root}/lib/controller_concerns]
 
     config.active_job.queue_adapter = :sidekiq
 
