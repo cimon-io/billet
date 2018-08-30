75d4df86f4646fbc399e0a6bb8c5c926d08a7e93
Alexey Osipenko <alexey@osipenko.in.ua>
2018-08-30 09:20:06 +0300

    Style/IfUnlessModifier

--- app/models/concerns/deny_destroy.rb
+++ app/models/concerns/deny_destroy.rb
@@ -9,9 +9,7 @@ module DenyDestroy
     def deny_destroy(opts = {})
       condition = opts.delete(:if) { raise ':if key is required' }
 
-      if condition.is_a?(Symbol)
-        return deny_destroy if: -> { send(condition) }
-      end
+      return deny_destroy if: -> { send(condition) } if condition.is_a?(Symbol)
 
       before_destroy do
         if instance_exec(&condition)
