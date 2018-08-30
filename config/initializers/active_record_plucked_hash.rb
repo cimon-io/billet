da7a915d7bf9615878594a0b2a1b1050ad3a0f68
Alexey Osipenko <alexey@osipenko.in.ua>
2018-08-30 09:16:26 +0300

    Layout/SpaceBeforeBlockBraces

--- config/initializers/active_record_plucked_hash.rb
+++ config/initializers/active_record_plucked_hash.rb
@@ -2,7 +2,7 @@ module ActiveRecordPluckedHash
   extend ActiveSupport::Concern
 
   def pluck_as_hash(*args)
-    pluck(*args).map{|r| Hash[args.zip(r)] }
+    pluck(*args).map {|r| Hash[args.zip(r)] }
   end
 end
 
